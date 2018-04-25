ab7c844a443a9912ed3d44863eb8fc5a92a10740
llr392425595 <392425595@qq.com>
2018-04-25 13:31:33 +0800

    重构section1 PracticeC

--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -1,17 +1,17 @@
 package cn.school.thoughtworks.section1;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PracticeC {
-    List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
-        List<String> result = new ArrayList<>();
-        for(String item : collection1){
-            for (List<String> childList : collection2.values()) {
-                if(childList.contains(item)){ result.add(item); }
-            }
-        }
-        return result;
-    }
+
+  List<String> collectSameElements(List<String> collection1,
+      Map<String, List<String>> collection2) {
+    List<String> valueListOfCollection2 = collection2.get("value");
+    return collection1.stream()
+        .filter(valueListOfCollection2::contains)
+        .collect(Collectors.toList());
+  }
+
 }
