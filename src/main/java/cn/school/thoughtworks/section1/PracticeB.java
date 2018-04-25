7d82848e94cc09a3c32d1a8afa3e716208e4aa60
llr392425595 <392425595@qq.com>
2018-04-25 13:26:54 +0800

    重构section1 PracticeB

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,16 +1,16 @@
 package cn.school.thoughtworks.section1;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PracticeB {
-    List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
-        List<String> result = new ArrayList<>();
-        for(String item : collection1){
-            for (List<String> childList : collection2){
-                if(childList.contains(item)){result.add(item);}
-            }
-        }
-        return result;
-    }
+
+  List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
+    List<String> flatCollection2 = collection2.stream()
+        .flatMap(Collection::stream)
+        .collect(Collectors.toList());
+    return collection1.stream().filter(flatCollection2::contains).collect(Collectors.toList());
+  }
 }
