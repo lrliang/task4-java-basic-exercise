98b500ff197a0a6a3c24b8ec1d45ad6769eed82c
llr392425595 <392425595@qq.com>
2018-04-25 13:37:20 +0800

    重构section2 PracticeA

--- src/main/java/cn/school/thoughtworks/section2/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeA.java
@@ -5,15 +5,11 @@ import java.util.List;
 import java.util.Map;
 
 public class PracticeA {
-    Map<String,Integer> countSameElements(List<String> collection1) {
-        Map<String,Integer> result = new HashMap<>();
-        for(String item : collection1){
-            if(result.get(item)==null){
-                result.put(item,1);
-            }else {
-                result.put(item,result.get(item)+1);
-            }
-        }
-        return result;
-    }
+
+  Map<String, Integer> countSameElements(List<String> collection1) {
+    Map<String, Integer> result = new HashMap<>();
+    collection1.stream()
+        .forEach(item -> result.put(item, result.get(item) == null ? 1 : result.get(item) + 1));
+    return result;
+  }
 }
