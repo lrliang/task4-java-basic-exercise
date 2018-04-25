460066b89cdb49f47ce95e564a361a17e4a29ecf
llr392425595 <392425595@qq.com>
2018-04-25 20:05:09 +0800

    section3 PracticeA删除多余依赖

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -1,21 +1,19 @@
 package cn.school.thoughtworks.section3;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeA {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
-        Map<String,Integer> result = collectionA;
         List<String> arr = object.get("value");
         for (Map.Entry<String, Integer> entry : collectionA.entrySet()) {
             String ch = entry.getKey();
             int count = entry.getValue();
             if(arr.contains(ch)){
-                if(count==1){ result.remove(ch); }
-                else { result.put(ch,count-1); }
+                if(count==1){ collectionA.remove(ch); }
+                else { collectionA.put(ch,count-1); }
             }
         }
-        return result;
+        return collectionA;
     }
 }
