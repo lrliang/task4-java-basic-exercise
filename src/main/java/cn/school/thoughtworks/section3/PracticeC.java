551ec1479bf5a3fe62f291a5741c41088806fd54
llr392425595 <392425595@qq.com>
2018-04-25 21:30:40 +0800

    重构section3 PracticB

--- src/main/java/cn/school/thoughtworks/section3/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeC.java
@@ -5,32 +5,32 @@ import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
-    private int FullThreeMinusOne(int count) {
-        int result = count;
-        if (count >= 3) { result = count - count / 3; }
-        return result;
+
+  private int FullThreeMinusOne(int count) {
+    return count >= 3 ? count - count / 3 : count;
+  }
+
+  private Map<String, Integer> mapListToObject(List<String> collection) {
+    Map<String, Integer> result = new HashMap<>();
+    for (String item : collection) {
+      result.merge(item, 1, (a, b) -> a + b);
+    }
+    return result;
+  }
+
+  Map<String, Integer> createUpdatedCollection(List<String> collectionA,
+      Map<String, List<String>> object) {
+    Map<String, Integer> result = new HashMap<>();
+    for (Map.Entry<String, Integer> entry : mapListToObject(collectionA).entrySet()) {
+      String ch = entry.getKey();
+      int count = entry.getValue();
+      if (!object.get("value").contains(ch)) {
+        result.put(ch, count);
+      } else {
+        result.put(ch, FullThreeMinusOne(count));
+      }
     }
 
-    private Map<String, Integer> mapListToObject(List<String> collection) {
-        Map<String, Integer> result = new HashMap<>();
-        for (String item : collection) {
-            if (result.get(item) == null) { result.put(item, 1); }
-            else { result.put(item, result.get(item) + 1); }
-        }
-        return result;
-    }
-
-    Map<String, Integer> createUpdatedCollection(List<String> collectionA, Map<String, List<String>> object) {
-        Map<String, Integer> result = new HashMap<>();
-        Map<String, Integer> collectionObject = mapListToObject(collectionA);
-        List<String> arr = object.get("value");
-        for (Map.Entry<String, Integer> entry : collectionObject.entrySet()) {
-            String ch = entry.getKey();
-            int count = entry.getValue();
-            if (!arr.contains(ch)) { result.put(ch, count); }
-            else { result.put(ch, FullThreeMinusOne(count)); }
-        }
-
-        return result;
-    }
+    return result;
+  }
 }
