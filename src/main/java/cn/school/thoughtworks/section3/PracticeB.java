e3aa6d2cb7966caa6b36c64d9c943160a13e9a82
llr392425595 <392425595@qq.com>
2018-04-25 21:06:59 +0800

    .

--- src/main/java/cn/school/thoughtworks/section3/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -4,21 +4,20 @@ import java.util.List;
 import java.util.Map;
 
 public class PracticeB {
-    public int FullThreeMinusOne(int count){
+    private int FullThreeMinusOne(int count){
         int result = count;
         if(count>=3){ result = count-count/3; }
         return result;
     }
 
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
-        Map<String,Integer> result = collectionA;
         List<String> arr = object.get("value");
         for (Map.Entry<String, Integer> entry : collectionA.entrySet()) {
             String ch = entry.getKey();
             int count = entry.getValue();
             if(arr.contains(ch)){
-                result.put(ch,FullThreeMinusOne(count));            }
+                collectionA.put(ch,FullThreeMinusOne(count));            }
         }
-        return result;
+        return collectionA;
     }
 }
